88a32c30857e7bf90a535e0ec89573be05a462d3
Tei <tei@radioactive.sg>
2020-11-22 13:22:16 +0700

    Initial Commit

---
+++ UICollection/UICollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICollectionApp.swift
+//  UICollection
+//
+//  Created by Tei on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct UICollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
